a1fc51322b800ac2ef927cca4a9cd95d9db487e9
lisashare <lisa_zhu2012@foxmail.com>
2019-12-10 20:53:19 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,4 @@
+
+[网易云音乐API](https://binaryify.github.io/NeteaseCloudMusicApi/#/?id=neteasecloudmusicapi)
+
+https://github.com/qqlcx5/uniapp/blob/master/components/header.vue
---
+++ components/headerTitle.vue
@@ -0,0 +1,19 @@
+<template>
+	<view class="header">
+		<view class="header-left" @click="goback()">
+			<image src="../static/icon_back@3x.png" mode=""></image>
+		</view>
+		<view class="header-content">
+			headertitle
+		</view>
+		<view class="header-right">
+			<!-- <image src="../../static/sousuo.png" mode=""></image> -->
+		</view>
+	</view>
+</template>
+
+<script>
+</script>
+
+<style>
+</style>
